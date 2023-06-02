4605ac63c76da1d820e5e768f9bd10f1ef8c14e3
Vitória Diniz <mariavitoriadz@gmail.com>
2023-06-02 13:19:31 +0000

    documentation.

--- cgi-bin/temperatura.py
+++ cgi-bin/temperatura.py
@@ -3,25 +3,51 @@
 import cgitb
 import cgi
 
+''' cgitb.enable: enables exception handling in HTML format for CGI errors.
+display=0: error details will only be logged in
+a log file, which is specified by logdir. '''
 cgitb.enable(display=0, logdir="./")
 
-def analyze_value(value):
-    if value:
-        return int(value)
-    return -1
 
+''' form: has all the data sent by the HTML form and -> form.getvalue(): gets the value
+associated with each field on the form.
+
+received: retrieves the value to be converted.
+unity1: retrieves the unit the value is in.
+unity2: retrieves the unit the value will be placed in.
+result_final: initializes the final result variable initially as an empty string.'''
 form = cgi.FieldStorage()
 received = form.getvalue('valor')
 unity1 = form.getvalue('unidade1')
 unity2 = form.getvalue('unidade2')
 result_final = ''
 
+def analyze_value(value):
+    '''analyze_value: converts the received value (value) into an integer.
+     return: returns -1 if the value cannot be converted.'''
+
+    if value:
+        return int(value)
+    return -1
+
 try:
+    '''analyze_value: parses the value of the 'value' parameter and if there is an exception
+    the value is defined as'typeError'.'''
     value = analyze_value(received)
 except:
     value = 'typeError'
 
 def convert_units(value, unity1, unity2):
+    '''convert_units: receives the value, unity1 and unity2 to perform checks and conversions
+     of the units.
+    
+    parameters:
+      value: value chosen to be converted.
+      unity1: unit in which the value is located.
+      unity2: unit in which the value should be converted.
+    
+    returns: returns error messages if values are invalid or the result of conversions.'''
+
     if value == -1:
         return 'Erro: Campos sem valores!'
     elif value == 'typeError':
@@ -53,10 +79,14 @@ def convert_units(value, unity1, unity2):
     return 'Erro: Selecione uma unidade!'
 
 try:
+    '''when performing unit conversion, if an exception occurs,
+     the value of result_final is defined as 'Erro Inesperado'.'''
     result_final = convert_units(value, unity1, unity2)
 except:
     result_final = 'Erro inesperado.'
 
+'''prints: print the header and body of the HTML response, displaying the 
+result and error message, if any.'''
 print("Content-type:text/html")
 print()
 print("<html>")
